90cb033730ec7b7fc5e9a452dcd1eab8b3537e21
victo <victor21420@gmail.com>
2023-08-17 23:01:58 -0300

    first commit

---
+++ MauiAppMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppMobile/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiAppMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        WebView webView;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Shell.SetTabBarIsVisible(this, false);
+            Shell.SetNavBarIsVisible(this, false);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            webView = new WebView();
+            Content = webView;
+
+
+            string url = "https://www.praticaeinovacao.dev.br/victorHugo";
+            webView.Source = new UrlWebViewSource { Url = url };
+        }
+    }
+}
